7bfecf45e24346c089a8f05ad07ebb96be91ceb3
mandersson1024 <mixand@gmail.com>
2019-05-17 17:05:28 +0200

    Experimenting with tkinter.

--- pyxrobots/src/main.py
+++ pyxrobots/src/main.py
@@ -1,3 +1,12 @@
 
+import tkinter as tk
+
+rot = tk.Tk()
+c = tk.Canvas(rot, width=640, height=640)
+c.create_text(100, 100, text="Hello, World!")
+c.grid()
+
+rot.mainloop()
+
 print("Hello, PyXRobots!")
 
