9673b0b2e4c1efa8e32773977abe09140c2c846b
mandersson1024 <mixand@gmail.com>
2019-07-07 13:00:54 +0200

    Fixed undo bug.

--- pyxrobots/src/main.py
+++ pyxrobots/src/main.py
@@ -9,47 +9,14 @@ show_main_menu: bool = True
 
 # if show_main_menu:
 #     output.display(main_menu_text)
+game_state = GameState(30, 22)
+game_state.player = (2, 0)
+game_state.enemies = [(0, 0)]
 
-m = Map(30, 22)
-# m.player_position = (2, 5)
-# m.enemy_positions = [(1, 1), (8, 8)]
-m.trash_piles = [(1, 3)]
-
-m.player_position = (2, 0)
-m.enemy_positions = [(0, 0)]
+m = Map()
+m.state = game_state
 
 undo_history = undo.UndoHistory()
-
-keys_up_left = ['q', '7']
-keys_up = ['w', '8']
-keys_up_right = ['e', '9']
-keys_left = ['a', '4']
-keys_pass = ['s', '5']
-keys_right = ['d', '6']
-keys_down_left = ['z', '1']
-keys_down = ['x', '2']
-keys_down_right = ['c', '3']
-
-
-def move_player_according_to_keypress(key: str) -> None:
-    if key in keys_up_left:
-        m.player_move_up_left()
-    elif key in keys_up:
-        m.player_move_up()
-    if key in keys_up_right:
-        m.player_move_up_right()
-    elif key in keys_left:
-        m.player_move_left()
-    elif key in keys_pass:
-        m.player_dont_move()
-    if key in keys_right:
-        m.player_move_right()
-    elif key in keys_down_left:
-        m.player_move_down_left()
-    if key in keys_down:
-        m.player_move_down()
-    elif key in keys_down_right:
-        m.player_move_down_right()
 
 
 def is_quit_key(key: str) -> bool:
@@ -81,39 +48,23 @@ def on_keypress(key: str) -> None:
         undo_history.redo()
 
     elif is_action_key(key):
-        old_player_position = m.player_position
-        old_enemy_positions = m.enemy_positions.copy()
-        old_trash_piles = m.trash_piles.copy()
-        print('old_enemy_positions' + str(old_enemy_positions))
+        old_state = m.state.clone()
+        new_state = m.state.clone()
 
-        move_player_according_to_keypress(key)
-        m.move_all_enemies()
-
-        new_player_position = m.player_position
-        new_enemy_positions = m.enemy_positions.copy()
-        new_trash_piles = m.trash_piles.copy()
-        print('new_enemy_positions' + str(new_enemy_positions))
+        new_state.move_player_according_to_keypress(key)
+        new_state.move_all_enemies()
 
         def do_function() -> None:
-            m.player_position = new_player_position
-            m.enemy_positions = new_enemy_positions
-            m.trash_piles = new_trash_piles
+            m.state = new_state
 
         def undo_function() -> None:
-            m.player_position = old_player_position
-            m.enemy_positions = old_enemy_positions
-            m.trash_piles = old_trash_piles
+            m.state = old_state
 
         step = undo.Command(do_function, undo_function)
         undo_history.do(step)
 
     window_ui.display(m.map_string)
 
-
-# TODO: Do 2 steps, undo 2 steps, redo 1 step.
-# TODO: Enemies move 2 steps.
-
-# TODO: Add class GameState with clone-function.
 
 # TODO: Add ControlActions constants and return them from UI instead of keycodes.
 
--- pyxrobots/src/map.py
+++ pyxrobots/src/map.py
@@ -1,13 +1,36 @@
 from utils import *
 
+keys_up_left = ['q', '7']
+keys_up = ['w', '8']
+keys_up_right = ['e', '9']
+keys_left = ['a', '4']
+keys_pass = ['s', '5']
+keys_right = ['d', '6']
+keys_down_left = ['z', '1']
+keys_down = ['x', '2']
+keys_down_right = ['c', '3']
 
-class Map:
 
-    def __init__(self, width: int, height: int):
+class GameState:
+    width: int
+    height: int
+    player: tuple
+    enemies: list
+    trash_piles: list
+
+    def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
-        self.player_position = (0, 0)
-        self.enemy_positions = []
+        self.player = (0, 0)
+        self.enemies = []
+        self.trash_piles = []
+
+    def clone(self):
+        state = GameState(self.width, self.height)
+        state.player = self.player
+        state.enemies = self.enemies.copy()
+        state.trash_piles = self.trash_piles.copy()
+        return state
 
     def is_position_within_bounds(self, pos) -> bool:
         x = pos[0]
@@ -15,9 +38,9 @@ class Map:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def player_move(self, dx, dy) -> None:
-        new_position = (self.player_position[0] + dx, self.player_position[1] + dy)
+        new_position = (self.player[0] + dx, self.player[1] + dy)
         if self.is_position_within_bounds(new_position):
-            self.player_position = new_position
+            self.player = new_position
 
     def player_move_up_left(self) -> None:
         self.player_move(-1, -1)
@@ -47,24 +70,51 @@ class Map:
         self.player_move(1, 1)
 
     def move_all_enemies(self) -> None:
-        for index in range(len(self.enemy_positions)):
+        for index in range(len(self.enemies)):
             self.move_enemy(index)
 
     def move_enemy(self, index):
-        enemy_pos = self.enemy_positions[index]
-        dx = self.player_position[0] - enemy_pos[0]
-        dy = self.player_position[1] - enemy_pos[1]
-        self.enemy_positions[index] = (enemy_pos[0] + sign(dx), enemy_pos[1] + sign(dy))
+        enemy_pos = self.enemies[index]
+        dx = self.player[0] - enemy_pos[0]
+        dy = self.player[1] - enemy_pos[1]
+        self.enemies[index] = (enemy_pos[0] + sign(dx), enemy_pos[1] + sign(dy))
+
+    def move_player_according_to_keypress(self, key: str) -> None:
+        if key in keys_up_left:
+            self.player_move_up_left()
+        elif key in keys_up:
+            self.player_move_up()
+        if key in keys_up_right:
+            self.player_move_up_right()
+        elif key in keys_left:
+            self.player_move_left()
+        elif key in keys_pass:
+            self.player_dont_move()
+        if key in keys_right:
+            self.player_move_right()
+        elif key in keys_down_left:
+            self.player_move_down_left()
+        if key in keys_down:
+            self.player_move_down()
+        elif key in keys_down_right:
+            self.player_move_down_right()
+
+
+class Map:
+    state: GameState
+
+    def __init__(self) -> None:
+        self.state = GameState(1, 1)
 
     @property
     def map_string(self) -> str:
         s = ""
 
-        for y in range(0, self.height):
-            for x in range(0, self.width):
-                if (x, y) == self.player_position:
+        for y in range(0, self.state.height):
+            for x in range(0, self.state.width):
+                if (x, y) == self.state.player:
                     s += '@'
-                elif (x, y) in self.enemy_positions:
+                elif (x, y) in self.state.enemies:
                     s += '+'
                 else:
                     s += '·'
