b10fbe5f7b58c4af450d305ecaca6b31e0b96ddb
mandersson1024 <mixand@gmail.com>
2019-07-30 19:59:50 +0200

    Now correct size of playing field.

--- pyxrobots/src/display_utils.py
+++ pyxrobots/src/display_utils.py
@@ -5,8 +5,8 @@ from ingame_data import *
 def ingame_data_to_map_string(data: IngameData) -> str:
     s = ''
 
-    for y in range(0, data.height - 1):
-        for x in range(0, data.width - 1):
+    for y in range(0, data.height):
+        for x in range(0, data.width):
             if (x, y) == data.player:
                 s += '@'
             elif (x, y) in data.enemies:
